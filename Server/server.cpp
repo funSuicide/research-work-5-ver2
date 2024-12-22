77de1054b81b4fff30137e9e534e5241f2401596
funSuicide <Digital-Corsair@yandex.com>
2024-12-22 14:54:54 +0400

    added changes

--- Server/server.cpp
+++ Server/server.cpp
@@ -2,5 +2,5 @@
 
 int main(int argc, char* argv[]) {
     Server server(1234, argv[1], argv[2]); 
-    server.start((unsigned char*)argv[3], (unsigned char*)argv[4]);
+    server.start((unsigned char*)argv[3], (unsigned char*)argv[4], argv[5]);
 }
